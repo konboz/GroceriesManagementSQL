9927b6d7e33c04d89cec7eded757583f977cd563
Konstantinos Bozikis <konstantinos.bozikis@yahoo.com>
2019-05-15 18:31:58 +0300

    Add project files.

---
+++ GroceriesManagementSQL/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace GroceriesManagementSQL
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
